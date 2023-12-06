08764afa273fa87b52040666da408ebec695ec27
Mustafa Pirbhai <mustafaspirbhai@gmail.com>
2023-12-07 00:34:36 +0300

    Add is_updater and is_controller fields to CustomUser model

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,10 +5,23 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
+
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username',]
+    list_display = ["email", "username", "is_updater"]
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (('Permissions'), {'fields': ('is_active', 'is_updater',"is_controller", 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'is_updater', "is_controller"),
+        }),
+    )
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class CustomUser(AbstractUser):
-    pass
+    is_updater = models.BooleanField(default=False, verbose_name="Updater Status")
+    is_controller  = models.BooleanField(default=False, verbose_name="Controller Status")
 
     def __str__(self):
         return self.email
